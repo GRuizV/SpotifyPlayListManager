b214fdd3a4e61958d74eaedccef4460620672175
Gerardo Ruiz <grville93@gmail.com>
2024-04-17 18:55:05 -0500

    Bukola YT Project Code Review Started

---
+++ References/Bukola YT PJ/main.py
@@ -0,0 +1,9 @@
+# Step 1: Log into Youtube
+# Step 2: Grab Liked Videos
+# Step 3: Create a New Playlist
+# Step 4: Search for the Song
+# Step 5: Add this son into the new Spotify playlist
+
+
+class CreatePlaylist:
+    pass
