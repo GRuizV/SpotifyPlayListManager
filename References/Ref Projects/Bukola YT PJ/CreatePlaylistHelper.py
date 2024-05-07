c327b7814be99a09dce595a7272abaf62f2bb583
Gerardo Ruiz <grville93@gmail.com>
2024-05-06 23:21:08 -0500

    The get_youtube_client function added. / Needs further testing to close

--- References/Ref Projects/Bukola YT PJ/CreatePlaylistHelper.py
+++ References/Ref Projects/Bukola YT PJ/CreatePlaylistHelper.py
@@ -5,10 +5,15 @@
 # Step 5: Add this son into the new Spotify playlist
 
 import os
-import json, requests, base64
+import json, requests
 import spotify_helper
 from dotenv import load_dotenv
-from datetime import datetime, timedelta
+import googleapiclient.errors
+import google_auth_oauthlib.flow
+import googleapiclient.discovery
+
+
+
 
 # Loading environmental variables which include clients secrets
 load_dotenv()
@@ -19,6 +24,7 @@ SPOTIFY_CLIENT_ID = os.getenv('SPOTIFY_CLIENT_ID')
 SPOTIFY_CLIENT_SECRET = os.getenv('SPOTIFY_CLIENT_SECRET')
 SPOTIFY_USER = os.getenv('SPOTIFY_USER')
 TOKENS_JSON_FILE_PATH = spotify_helper.TOKENS_JSON_FILE_PATH
+YT_JSON_FILE_PATH = r'C:\Users\USUARIO\GR\Software Development\Projects\Spotify Playlists Manager\References\Ref Projects\Bukola YT PJ\youtube_client_secrets.json'
 
 
 
@@ -28,9 +34,30 @@ class CreatePlaylist:
     
     # Step 1: Log into Youtube
     @staticmethod
-    def get_youtube_client():
-        pass
+    def get_youtube_client() -> googleapiclient.discovery.build:
+
+        '''This function creates a Youtube service which authenticates and is able to make API calls to Youtube server'''
         
+        # *** Copied from Youtube's Data API Documentation
+        # Disable OAuthlib's HTTPS verification when running locally.
+        # *DO NOT* leave this option enabled in production.
+        os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "0"
+
+        api_service_name = "youtube"
+        api_version = "v3"
+        client_secrets_file = YT_JSON_FILE_PATH
+
+        # Get credentials and create an API client
+        scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
+        flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(client_secrets_file, scopes)
+        credentials = flow.run_local_server(port=0)        
+
+        #from the Youtube DATA API
+        youtube_client = googleapiclient.discovery.build(api_service_name, api_version, credentials=credentials)
+
+        return youtube_client
+
+
 
     # Step 2: Grab Liked Videos
     @staticmethod
@@ -135,7 +162,7 @@ class CreatePlaylist:
 
 
 
-# # 'get_spotify_uri' functions testing
+"'get_spotify_uri' functions testing"
 # art = 'Granuja'
 # song = 'Días de Perros'
 
@@ -143,6 +170,32 @@ class CreatePlaylist:
 
 # test = CreatePlaylist.get_spotify_uri(artist=art, song_name=song)
 # print(test)
+
+
+
+
+
+'Accessing to the YT client secrets'
+# with open(YT_JSON_FILE_PATH) as f:
+    
+#     data = json.load(f)
+
+#     client_id = data['installed']['client_id']
+#     client_secret = data['installed']['client_secret']
+
+#     print(client_id)
+#     print(client_secret)
+
+
+
+
+"Testing the 'get_youtube_client' function"
+
+new_service = CreatePlaylist.get_youtube_client()
+
+print(new_service)
+
+
 
 
 
